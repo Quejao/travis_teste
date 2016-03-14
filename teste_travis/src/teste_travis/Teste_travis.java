a5af49a69cb31453890d2dc4c63a9b8f6d516498
Quejao <Leonardo@192.168.237.6>
2016-03-14 11:38:49 -0300

    Criando ola mundo

--- teste_travis/src/teste_travis/Teste_travis.java
+++ teste_travis/src/teste_travis/Teste_travis.java
@@ -16,6 +16,7 @@ public class Teste_travis {
      */
     public static void main(String[] args) {
       System.out.println("Hello Word!");
+      System.out.println("Ola mundo!");      
     }
     
 }
